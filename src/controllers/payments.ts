e5b28e186fafe70398a362adc69dfb6bc82625bf
Daniil Volosevich <Volosevichdan@gmail.com>
2022-10-30 18:49:49 +0300

    payments ready to go

--- src/controllers/payments.ts
+++ src/controllers/payments.ts
@@ -1,6 +1,14 @@
 import {Response, Request} from "express";
 import {dataSource} from "../data-source";
 import {PaymentEntity} from "../entity/Payment.entity";
+import { CreditEntity } from "../entity/Credit.entity";
+
+type PaymentBodyType = {
+    Title: string,
+    Amount: number,
+    Date: string,
+    creditID: number
+}
 
 export const getPayments = async (req: Request, res: Response) => {
     const paymentsRep = dataSource.getRepository(PaymentEntity)
@@ -12,4 +20,56 @@ export const getPayments = async (req: Request, res: Response) => {
     })
 
     res.json(payments)
+}
+
+export const createPayment = async (req: Request, res: Response) => {
+    try {
+        const paymentRep = await dataSource.getRepository(PaymentEntity);
+        const creditRep = await dataSource.getRepository(CreditEntity);
+        const credit = await creditRep.findOne({where: {
+            ID: req.body.creditID
+        }})
+        if(!credit) {
+            return res.status(400).send('There is no such credit')
+        }
+        const payment = await paymentRep.create({...req.body, Credit: credit});
+        const results = await dataSource.getRepository(PaymentEntity).save(payment)
+        return res.status(200).json(results)
+    } catch (err) {
+        console.error(err)
+        res.status(500).end()
+    }
+}
+
+export const changePayment = async (req: Request, res: Response) => {
+    try {
+        const paymentRep = await dataSource.getRepository(PaymentEntity);
+        const creditRep = await dataSource.getRepository(CreditEntity);
+
+        const credit = await creditRep.findOne({where: {
+            ID: req.body.creditID
+        }})
+        if(!credit) {
+            return res.status(400).send('There is no such credit')
+        }
+
+        delete req.body.creditID
+
+        const results = await paymentRep.update(req.params.id, { ...req.body, Credit: credit})
+        return res.status(200).json(results)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+export const deletePayment = async (req: Request, res: Response) => 
+{
+    try {
+        const paymentRep = await dataSource.getRepository(PaymentEntity);
+        const result = await paymentRep.delete(req.params.id)
+        res.status(200).json(result)
+    } catch (err) {
+        console.log(err)
+        res.status(500).end()
+    }
 }
--- src/routes/payments.ts
+++ src/routes/payments.ts
@@ -1,8 +1,14 @@
 import {Router} from 'express'
-import {getPayments} from "../controllers/payments";
+import {getPayments, createPayment, deletePayment, changePayment} from "../controllers/payments";
 
 const router = Router()
 
 router.get('/', getPayments)
+
+router.post('/', createPayment);
+
+router.delete('/:id', deletePayment)
+
+router.put('/:id', changePayment)
 
 export default router
